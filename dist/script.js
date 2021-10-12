718ef5513f563ab2cdd600d7b497847a8473b9d1
Dima <laskovdmit@gmail.com>
2021-10-12 09:49:36 +0500

    Добавлена возможность просмотра картинок в модальном окне

--- dist/script.js
+++ dist/script.js
@@ -15154,6 +15154,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/forms */ "./src/js/modules/forms.js");
 /* harmony import */ var _modules_changeModalState__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/changeModalState */ "./src/js/modules/changeModalState.js");
 /* harmony import */ var _modules_timer__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/timer */ "./src/js/modules/timer.js");
+/* harmony import */ var _modules_images__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./modules/images */ "./src/js/modules/images.js");
+
 
 
 
@@ -15170,6 +15172,7 @@ window.addEventListener('DOMContentLoaded', () => {
   Object(_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   Object(_modules_forms__WEBPACK_IMPORTED_MODULE_3__["default"])(modalState);
   Object(_modules_timer__WEBPACK_IMPORTED_MODULE_5__["default"])('#timer', deadline);
+  Object(_modules_images__WEBPACK_IMPORTED_MODULE_6__["default"])();
 });
 
 /***/ }),
@@ -15376,6 +15379,50 @@ const forms = state => {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (forms);
+
+/***/ }),
+
+/***/ "./src/js/modules/images.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/images.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const images = () => {
+  const imgPopup = document.createElement('div'),
+        workSection = document.querySelector('.works'),
+        bigImage = document.createElement('img');
+  imgPopup.classList.add('popup');
+  workSection.appendChild(imgPopup);
+  imgPopup.style.cssText = `
+        justify-content: center;
+        align-items: center;
+        display: none;
+    `;
+  imgPopup.appendChild(bigImage);
+  workSection.addEventListener('click', e => {
+    e.preventDefault();
+    let target = e.target;
+
+    if (target && target.classList.contains('preview')) {
+      imgPopup.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+      const path = target.parentNode.getAttribute('href');
+      bigImage.setAttribute('src', path);
+      bigImage.setAttribute('height', window.innerHeight * 0.8);
+    }
+
+    if (target && target.classList.contains('popup')) {
+      imgPopup.style.display = 'none';
+      document.body.style.overflow = '';
+    }
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (images);
 
 /***/ }),
 
---
+++ src/js/modules/images.js
@@ -0,0 +1,39 @@
+const images = () => {
+    const imgPopup = document.createElement('div'),
+          workSection = document.querySelector('.works'),
+          bigImage = document.createElement('img');
+
+    imgPopup.classList.add('popup');
+    workSection.appendChild(imgPopup);
+
+    imgPopup.style.cssText = `
+        justify-content: center;
+        align-items: center;
+        display: none;
+    `;
+
+    imgPopup.appendChild(bigImage);
+
+    workSection.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let target = e.target;
+
+        if (target && target.classList.contains('preview')) {
+            imgPopup.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+
+            const path = target.parentNode.getAttribute('href');
+            bigImage.setAttribute('src', path);
+            bigImage.setAttribute('height', window.innerHeight * 0.8);
+        }
+
+        if (target && target.classList.contains('popup')) {
+            imgPopup.style.display = 'none';
+            document.body.style.overflow = '';
+        }
+    });
+
+};
+
+export default images;
